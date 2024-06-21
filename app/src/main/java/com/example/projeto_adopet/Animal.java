2d8c38d2188adc7d450f5c15dd9bf4bc30578ac7
Julio Sena <juliosena@hotmail.com>
2024-06-20 21:37:23 -0300

    Start commit!

--- app/src/main/java/com/example/projeto_adopet/Animal.java
+++ app/src/main/java/com/example/projeto_adopet/Animal.java
@@ -1,25 +1,29 @@
 package com.example.projeto_adopet;
 
-public class Pet {
-    private int imageResId;
+public class Animal {
+    private int adopet;
     private String name;
     private String age;
     private String breed;
     private String color; // Adicione este atributo
 
-    public Pet(int imageResId, String name, String age, String breed, String color) {
-        this.imageResId = imageResId;
+    public Animal(int adopet, String name, String age, String breed, String color) {
+        this.adopet = adopet;
         this.name = name;
         this.age = age;
         this.breed = breed;
         this.color = color;
     }
 
-    public Pet(int icPetDog2, String bobby, String age, String srd) {
+    public Animal(int adopet, String bobby, String age, String srd) {
     }
 
-    public int getImageResId() {
-        return imageResId;
+    public Animal(String nome, String raca, String genero, String tamanho, String peso, String idade, String tipo) {
+
+    }
+
+    public int getAdopet() {
+        return adopet;
     }
 
     public String getName() {
--- app/src/main/java/com/example/projeto_adopet/MainActivity.java
+++ app/src/main/java/com/example/projeto_adopet/MainActivity.java
@@ -7,20 +7,17 @@ import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static int SPLASH_TIME_OUT = 5000;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent homeIntent = new Intent(MainActivity.this, Login.class);
-                startActivity(homeIntent);
-                finish();
-            }
+        final int SPLASH_TIME_OUT = 5000; // Tempo de espera de 5 segundos
+
+        new Handler().postDelayed(() -> {
+            Intent homeIntent = new Intent(MainActivity.this, Login.class);
+            startActivity(homeIntent);
+            finish();
         }, SPLASH_TIME_OUT);
     }
 }
